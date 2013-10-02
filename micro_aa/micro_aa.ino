7e6e56ed4cf0bb1dcb439604f3786fd5dca55e25
Thanasi <thanasi91@gmail.com>
2013-10-02 12:37:39 -0400

    few more updates and housekeeping. also going to try to take care of background signal in the processing

--- micro_aa/micro_aa.ino
+++ micro_aa/micro_aa.ino
@@ -15,59 +15,92 @@ void setup() {
 } 
 
 void loop() { 
-  int redOut, irOut, i; 
-  float ratio;
- 
-  // measure the two wavelengths with a short delay between them
+  int i; 
+  float irOut, redOut, bgOut, ratio;
+
+  bgOut = readingBG();
+  delay(2)
+  // measure the RMS transmission for the two wavelengths 
+  // with a short delay between them
   redOut = readingRED(); 
-  delay(5)
+  delay(3);
   irOut = readingIR(); 
+
+  // subtract off average background levels
+  redOut -= bgOut;
+  irOut -= bgOut;
  
-  ratio = calculateSPO2(redOut, irOut); 
+  ratio = calculateSPO2(irOut, redOut); 
  
-  Serial.print(redOut); 
-  Serial.print(","); 
+  Serial.print(redOut);
+  Serial.print("\t"); 
   Serial.print(irOut); 
-  Serial.print(","); 
+  Serial.print("\t"); 
   Serial.println(ratio); 
 } 
 
+
+float readingBG() { 
+  //--Initializations-- 
+  int raw, i; 
+  float avg = 0;
+ 
+  digitalWrite(IR,HIGH);   // turn off IR LED 
+  digitalWrite(RED,HIGH);  // turn off RED LED 
+
+  for (i=0;i<AVGBUFFER;i++){         // start running average 
+    raw = analogRead(inputPin);      // read photodiode signal
+    avg +=  (float) raw / AVGBUFFER; 
+  } 
+
+  return avg;             // return total average 
+}
+
+
 float readingRED() { 
   //--Initializations-- 
-  float raw, i; 
-  int avgRED = 0;
+  int raw, i; 
+  float avg = 0;
  
-  analogWrite(RED, 60);           // turn on RED LED 
-  for (i=0;i<AVGBUFFER;i++){      // start running average 
-    raw = analogRead(inputPin);   // read photodiode signal 
-    avgRED += raw;                // contribute to average 
+  analogWrite(RED, 60);               // turn on RED LED 
+  for (i=0;i<20;i++){                 // don't actually collect until the signal is in a steady state
+    raw = analogRead(inputPin);       // read photodiode signal 
+  } 
+  for (i=0;i<AVGBUFFER-20;i++){       // start running average 
+    raw = analogRead(inputPin);       // read photodiode signal 
+    avg += (float) raw / AVGBUFFER;   // contribute to average
   } 
  
-  digitalWrite(RED,HIGH);              //turn off RED LED 
-  return (float) avgRED / AVGBUFFER;   // return total average 
+  digitalWrite(RED,HIGH);   // turn off RED LED 
+  return avg;               // return total average 
 } 
 
 float readingIR() { 
   //--Initializations-- 
-  int raw, i;
-  int avgIR = 0;
+  int raw, i; 
+  float avg = 0;
  
-  analogWrite(IR, 250);           // turn on IR LED 
-  for (i=0;i<AVGBUFFER;i++){      // start running average 
-    raw = analogRead(inputPin);   // read photodiode signal 
-    avgIR += raw;                 // contribute to average 
+  analogWrite(IR, 60);               // turn on IR LED 
+  for (i=0;i<20;i++){                 // don't actually collect until the signal is in a steady state
+    raw = analogRead(inputPin);       // read photodiode signal 
   } 
- 
-  digitalWrite(IR,HIGH);              // turn off IR LED 
-  return (float) avgIR / AVGBUFFER;   // return total average 
-} 
+  for (i=0;i<AVGBUFFER-20;i++){       // start running average 
+    raw = analogRead(inputPin);       // read photodiode signal 
+    avg += (float) raw / AVGBUFFER;   // contribute to average
+  } 
+  
+  digitalWrite(IR,HIGH);  // turn off IR LED 
+  return avg;             // return total average 
+}
 
-float calculateSPO2(float rawIR, float rawRED) { 
+
+float calculateSPO2(float sigIR, float sigRED) { 
   
-  float rmsIR, rmsRED, ratio;
-  rmsIR = rawIR/sqrt(2);
-  rmsRED = rawRED/sqrt(2); 
-  ratio = log10(rmsRED)/log10(rmsIR); 
+  float ratio, rmsRED, rmsIR;
+  rmsRED = .5 * sigRED / sqrt(2);
+  rmsIR = .5 * sigIR / sqrt(2);
+
+  ratio = log10(rmsRED) / log10(rmsIR); 
+  
   return ratio; 
-  
 }
